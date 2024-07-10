cc7da01b151168a583161cef3d26ceff2bb8d5b8
romanmt <romanmt@gmail.com>
2024-07-10 15:55:36 -0400

    added the ability to start and end a conversation

--- lib/lean_coffee/storage.ex
+++ lib/lean_coffee/storage.ex
@@ -10,7 +10,7 @@ defmodule LeanCoffee.Storage do
   end
 
   def create_session(id, name) do
-    :ets.insert(@table_name, {id, name, [], []})
+    :ets.insert(@table_name, {id, name, [], [], nil, nil})
     Phoenix.PubSub.broadcast(LeanCoffee.PubSub, "session:#{id}", {:new_session, id, name})
   end
 
@@ -20,9 +20,13 @@ defmodule LeanCoffee.Storage do
 
   def join_session(session_id, user) do
     case :ets.lookup(@table_name, session_id) do
-      [{^session_id, name, participants, topics}] ->
+      [{^session_id, name, participants, topics, current_topic, timer_ref}] ->
         new_participants = Enum.uniq([user | participants])
-        :ets.insert(@table_name, {session_id, name, new_participants, topics})
+
+        :ets.insert(
+          @table_name,
+          {session_id, name, new_participants, topics, current_topic, timer_ref}
+        )
 
         Phoenix.PubSub.broadcast(
           LeanCoffee.PubSub,
@@ -30,18 +34,22 @@ defmodule LeanCoffee.Storage do
           {:update_participants, session_id, new_participants}
         )
 
-        {:ok, {session_id, name, new_participants, topics}}
+        {:ok, {session_id, name, new_participants, topics, current_topic, timer_ref}}
 
       [] ->
         {:error, :session_not_found}
     end
   end
 
-  def propose_topic(session_id, topic) do
+  def propose_topic(session_id, topic, user) do
     case :ets.lookup(@table_name, session_id) do
-      [{^session_id, name, participants, topics}] ->
-        new_topics = [{topic, 0} | topics]
-        :ets.insert(@table_name, {session_id, name, participants, new_topics})
+      [{^session_id, name, participants, topics, current_topic, timer_ref}] ->
+        new_topics = [{topic, 0, user} | topics]
+
+        :ets.insert(
+          @table_name,
+          {session_id, name, participants, new_topics, current_topic, timer_ref}
+        )
 
         Phoenix.PubSub.broadcast(
           LeanCoffee.PubSub,
@@ -49,7 +57,7 @@ defmodule LeanCoffee.Storage do
           {:new_topic, session_id, new_topics}
         )
 
-        {:ok, {session_id, name, participants, new_topics}}
+        {:ok, {session_id, name, participants, new_topics, current_topic, timer_ref}}
 
       [] ->
         {:error, :session_not_found}
@@ -57,15 +65,20 @@ defmodule LeanCoffee.Storage do
   end
 
   def vote_topic(session_id, topic) do
+    IO.puts("**** voting on #{topic}")
+
     case :ets.lookup(@table_name, session_id) do
-      [{^session_id, name, participants, topics}] ->
+      [{^session_id, name, participants, topics, current_topic, timer_ref}] ->
         new_topics =
           Enum.map(topics, fn
-            {^topic, votes} -> {topic, votes + 1}
+            {^topic, votes, user} -> {topic, votes + 1, user}
             other -> other
           end)
 
-        :ets.insert(@table_name, {session_id, name, participants, new_topics})
+        :ets.insert(
+          @table_name,
+          {session_id, name, participants, new_topics, current_topic, timer_ref}
+        )
 
         Phoenix.PubSub.broadcast(
           LeanCoffee.PubSub,
@@ -73,7 +86,45 @@ defmodule LeanCoffee.Storage do
           {:update_topics, session_id, new_topics}
         )
 
-        {:ok, {session_id, name, participants, new_topics}}
+        {:ok, {session_id, name, participants, new_topics, current_topic, timer_ref}}
+
+      [] ->
+        {:error, :session_not_found}
+    end
+  end
+
+  def start_discussion(session_id, topic) do
+    case :ets.lookup(@table_name, session_id) do
+      [{^session_id, name, participants, topics, _current_topic, _timer_ref}] ->
+        # 5 minutes
+        new_timer_ref = Process.send_after(self(), {:end_discussion, session_id}, 5 * 60 * 1000)
+        :ets.insert(@table_name, {session_id, name, participants, topics, topic, new_timer_ref})
+
+        Phoenix.PubSub.broadcast(
+          LeanCoffee.PubSub,
+          "session:#{session_id}",
+          {:start_discussion, session_id, topic}
+        )
+
+        {:ok, {session_id, name, participants, topics, topic, new_timer_ref}}
+
+      [] ->
+        {:error, :session_not_found}
+    end
+  end
+
+  def end_discussion(session_id) do
+    case :ets.lookup(@table_name, session_id) do
+      [{^session_id, name, participants, topics, current_topic, _timer_ref}] ->
+        :ets.insert(@table_name, {session_id, name, participants, topics, nil, nil})
+
+        Phoenix.PubSub.broadcast(
+          LeanCoffee.PubSub,
+          "session:#{session_id}",
+          {:end_discussion, session_id, current_topic}
+        )
+
+        {:ok, {session_id, name, participants, topics, nil, nil}}
 
       [] ->
         {:error, :session_not_found}
@@ -82,8 +133,8 @@ defmodule LeanCoffee.Storage do
 
   def get_session(session_id) do
     case :ets.lookup(@table_name, session_id) do
-      [{^session_id, name, participants, topics}] ->
-        {:ok, {session_id, name, participants, topics}}
+      [{^session_id, name, participants, topics, current_topic, timer_ref}] ->
+        {:ok, {session_id, name, participants, topics, current_topic, timer_ref}}
 
       [] ->
         {:error, :session_not_found}
@@ -96,5 +147,11 @@ defmodule LeanCoffee.Storage do
   def init(:ok) do
     :ets.new(@table_name, [:named_table, :public, :set])
     {:ok, %{}}
+  end
+
+  @impl true
+  def handle_info({:end_discussion, session_id}, state) do
+    end_discussion(session_id)
+    {:noreply, state}
   end
 end
--- lib/lean_coffee_web/live/session_live/show.ex
+++ lib/lean_coffee_web/live/session_live/show.ex
@@ -7,15 +7,17 @@ defmodule LeanCoffeeWeb.SessionLive.Show do
     if connected?(socket), do: Phoenix.PubSub.subscribe(LeanCoffee.PubSub, "session:#{id}")
 
     case Storage.get_session(id) do
-      {:ok, {session_id, name, participants, topics}} ->
+      {:ok, {session_id, name, participants, topics, current_topic, _time_ref}} ->
         {:ok,
          assign(socket,
            session_id: session_id,
            name: name,
            participants: participants,
            topics: topics,
-           new_user: "",
-           new_topic: ""
+           current_topic: current_topic,
+           user_name: nil,
+           new_topic: "",
+           discussion_time: 5
          )}
 
       {:error, :session_not_found} ->
@@ -23,38 +25,68 @@ defmodule LeanCoffeeWeb.SessionLive.Show do
     end
   end
 
-  def handle_event("join", %{"user" => user}, socket) do
-    case Storage.join_session(socket.assigns.session_id, user) do
-      {:ok, {_session_id, _name, participants, _topics}} ->
-        {:noreply, assign(socket, participants: participants, new_user: "")}
+  def handle_event("submit_user_name", %{"user_name" => user_name}, socket) do
+    user_name = String.trim(user_name)
 
-      {:error, :session_not_found} ->
-        {:noreply, socket |> put_flash(:error, "Session not found") |> redirect(to: "/")}
+    if user_name == "" do
+      {:noreply, socket |> put_flash(:error, "Name cannot be empty")}
+    else
+      {:noreply, assign(socket, user_name: user_name)}
     end
   end
 
-  def handle_event("update_new_user", %{"user" => new_user}, socket) do
-    {:noreply, assign(socket, new_user: new_user)}
+  def handle_event("update_user_name", %{"user_name" => user_name}, socket) do
+    {:noreply, assign(socket, user_name: user_name)}
   end
 
-  def handle_event("propose_topic", %{"topic" => topic}, socket) do
-    case Storage.propose_topic(socket.assigns.session_id, topic) do
-      {:ok, {_session_id, _name, _participants, topics}} ->
-        {:noreply, assign(socket, topics: topics, new_topic: "")}
-
-      {:error, :session_not_found} ->
-        {:noreply, socket |> put_flash(:error, "Session not found") |> redirect(to: "/")}
-    end
-  end
-
-  def handle_event("update_new_topic", %{"topic" => new_topic}, socket) do
+  def handle_event("update_new_topic", %{"new_topic" => new_topic}, socket) do
     {:noreply, assign(socket, new_topic: new_topic)}
+  end
+
+  def handle_event("propose_topic", %{"new_topic" => topic}, socket) do
+    user_name = socket.assigns.user_name
+
+    if user_name == "" do
+      {:noreply, socket |> put_flash(:error, "Please enter your name before proposing a topic.")}
+    else
+      case Storage.propose_topic(socket.assigns.session_id, topic, user_name) do
+        {:ok, {_session_id, _name, _participants, topics, _current_topic, _timer_ref}} ->
+          {:noreply, assign(socket, topics: topics, new_topic: "")}
+
+        {:error, :session_not_found} ->
+          {:noreply, socket |> put_flash(:error, "Session not found") |> redirect(to: "/")}
+      end
+    end
   end
 
   def handle_event("vote", %{"topic" => topic}, socket) do
     case Storage.vote_topic(socket.assigns.session_id, topic) do
-      {:ok, {_session_id, _name, _participants, topics}} ->
+      {:ok, {_session_id, _name, _participants, topics, _current_topic, _timer_ref}} ->
         {:noreply, assign(socket, topics: topics)}
+
+      {:error, :session_not_found} ->
+        {:noreply, socket |> put_flash(:error, "Session not found") |> redirect(to: "/")}
+    end
+  end
+
+  def handle_event("start_discussion", %{"topic" => topic}, socket) do
+    IO.puts("***** starting discussion #{topic}")
+
+    case Storage.start_discussion(socket.assigns.session_id, topic) do
+      {:ok, {_session_id, _name, _participants, _topics, current_topic, _timer_ref}} ->
+        {:noreply, assign(socket, current_topic: current_topic)}
+
+      {:error, :session_not_found} ->
+        {:noreply, socket |> put_flash(:error, "Session not found") |> redirect(to: "/")}
+    end
+  end
+
+  def handle_event("end_discussion", _params, socket) do
+    IO.puts("***** ending discussion")
+
+    case Storage.end_discussion(socket.assigns.session_id) do
+      {:ok, {_session_id, _name, _participants, _topics, _current_topic, _timer_ref}} ->
+        {:noreply, assign(socket, current_topic: nil)}
 
       {:error, :session_not_found} ->
         {:noreply, socket |> put_flash(:error, "Session not found") |> redirect(to: "/")}
@@ -71,5 +103,13 @@ defmodule LeanCoffeeWeb.SessionLive.Show do
 
   def handle_info({:update_topics, _session_id, topics}, socket) do
     {:noreply, assign(socket, topics: topics)}
+  end
+
+  def handle_info({:start_discussion, _session_id, topic}, socket) do
+    {:noreply, assign(socket, current_topic: topic)}
+  end
+
+  def handle_info({:end_discussion, _session_id, _topic}, socket) do
+    {:noreply, assign(socket, current_topic: nil)}
   end
 end
